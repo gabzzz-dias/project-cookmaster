0cb97e28d8f1d5b73a80514198d52b1ab5d663c1
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-10-04 00:23:02 -0300

    fix connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,11 +3,19 @@ const { MongoClient } = require('mongodb');
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
+let schema = null;
+
 const connection = () => {
-    MongoClient.connect(MONGO_DB_URL,
+  if (schema) return Promise.resolve(schema);
+
+  return MongoClient.connect(MONGO_DB_URL,
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
     .catch((err) => {
       console.error(err);
     });
